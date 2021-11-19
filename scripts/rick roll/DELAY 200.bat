c5b5351e674b78d63f427be6ef08619e941d6a72
nnnolan <89788103+nnnolan@users.noreply.github.com>
2021-11-19 08:16:56 -0600

    Create DELAY 200.bat

---
+++ scripts/rick roll/DELAY 200.bat
@@ -0,0 +1,22 @@
+REM Title : Northland CAPS Rick Roll
+REM Author : Northland CAPS :)
+REM Description : Rick Roll
+REM Date : 11/17/21
+DELAY 200
+GUI r 
+DELAY 50
+STRING powershell.exe
+DELAY 100
+ENTER
+DELAY 50
+STRING Function Set-Speaker($Volume){$wshShell = new-object -com wscript.shell;1..50 | % {$wshShell.SendKeys([char]174)};1..$Volume | % {$wshShell.SendKeys([char]175)}}
+ENTER
+DELAY 50
+String Set-Speaker -Volume 10
+DELAY 100
+ENTER
+DELAY 200
+GUI r
+DELAY 50
+STRING https://www.youtube.com/watch?v=xvFZjo5PgG0
+ENTER
